dd2a91aaf77a9d24ac52f581e42537e8e95d637a
Suhanto Suwardana Dharmasaputra <dsledlight2408.com>
2024-09-10 14:03:34 +0700

    Ini Tugas Pertama

---
+++ 10092024/Tes.sh
@@ -0,0 +1,33 @@
+# echo "Siapa nama Anda?"
+# read name
+# echo "Nama saya adalah $name"
+
+
+# read -p "Masukkan sisi : " s
+# add=$((s*s))
+# echo "Luas dari persegi tersebut adalah : "$add
+
+
+# read -p "Masukkan berat : " m
+# read -p "Masukkan kecepatan : " a
+# echo "Gaya (F) = "$((m*a))
+
+
+# HitungLuas(){
+#     read -p "Masukkan sisi : " s
+#     echo "Luas = "$((s * s))
+# }
+# HitungLuas
+
+# Alamat(){
+#     echo "Saya tinggal di Jakarta"
+# }
+# Alamat
+
+KelSeg(){
+    read -p "Masukkan sisi 1 : " a
+    read -p "Masukkan sisi 2 : " b
+    read -p "Masukkan siis 3 : " c
+    echo "Keliling segitiga : "$((a + b + c))
+}
+KelSeg
